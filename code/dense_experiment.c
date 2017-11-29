8c8172d3f008279febfd5e2458a96f6d86d72c8b
evcu <evcu@users.noreply.github.com>
2017-11-29 16:12:15 -0500

    makefile fixed, lets compile programs!

--- code/dense_experiment.c
+++ code/dense_experiment.c
@@ -7,26 +7,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+
 /* The input is 2d 2d
  */
 
-// void gpu_blas_mmul(const float *A, const float *B, float *C, const int m, const int k, const int n) {
-//    int lda=m,ldb=k,ldc=m;
-//    const float alf = 1;
-//    const float bet = 0;
-//    const float *alpha = &alf;
-//    const float *beta = &bet;
-//
-//    // Create a handle for CUBLAS
-//    cublasHandle_t handle;
-//    cublasCreate(&handle);
-//
-//    // Do the actual multiplication
-//    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
-//
-//    // Destroy the handle
-//    cublasDestroy(handle);
-// }
+void gpu_blas_mmul(const float *A, const float *B, float *C, const int m, const int k, const int n) {
+   int lda=m,ldb=k,ldc=m;
+   const float alf = 1;
+   const float bet = 0;
+   const float *alpha = &alf;
+   const float *beta = &bet;
+
+   // Create a handle for CUBLAS
+   cublasHandle_t handle;
+   cublasCreate(&handle);
+
+   // Do the actual multiplication
+   cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+
+   // Destroy the handle
+   cublasDestroy(handle);
+}
 
 void mm_cpu(float *a, float *b, float *c, int m, int n, int k){
     int i,j,r;
