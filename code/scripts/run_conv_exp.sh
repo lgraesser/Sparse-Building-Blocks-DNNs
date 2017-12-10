d3de554e8bf34bc4b7e34559571650ca83353baa
Laura Graesser <lhg256@linax1.cims.nyu.edu>
2017-12-10 18:48:06 -0500

    Generating results script

---
+++ code/scripts/run_conv_exp.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+matrices=("128.mat" "224.mat" "512.mat" "1024.mat" "2048.mat")
+k3=("k3_0.mat" "k3_05.mat" "k3_09.mat" "k3_095.mat" "k3_099.mat")
+k5=("k5_0.mat" "k5_05.mat" "k5_09.mat" "k5_095.mat" "k5_099.mat")
+k7=("k7_0.mat" "k7_05.mat" "k7_09.mat" "k7_095.mat" "k7_099.mat")
+algos=("projdense" "projdensepitch" "densecudnn" "sparse" "sparsepitch")
+
+for i in "${matrices[@]}"
+do
+  echo "===================== $i MATRIX =================="
+  for j in "${k3[@]}"
+  do
+    echo "===================== $j KERNEL =================="
+    for k in "${algos[@]}"
+    do
+      echo "===================== $k ALGORITHM =================="
+      echo "===================== 1 iteration =================="
+      ../bin/conv_exp -a $k $i $j
+      echo "===================== 1000 iterations =================="
+      ../bin/conv_exp -n -a $k $i $j
+    done
+  done
+  for j in "${k5[@]}"
+  do
+    echo "===================== $j KERNEL =================="
+    for k in "${algos[@]}"
+    do
+      echo "===================== $k ALGORITHM =================="
+      echo "===================== 1 iteration =================="
+      ../bin/conv_exp -a $k $i $j
+      echo "===================== 1000 iterations =================="
+      ../bin/conv_exp -n -a $k $i $j
+    done
+  done
+  for j in "${k7[@]}"
+  do
+    echo "===================== $j KERNEL =================="
+    for k in "${algos[@]}"
+    do
+      echo "===================== $k ALGORITHM =================="
+      echo "===================== 1 iteration =================="
+      ../bin/conv_exp -a $k $i $j
+      echo "===================== 1000 iterations =================="
+      ../bin/conv_exp -n -a $k $i $j
+    done
+  done
+done
