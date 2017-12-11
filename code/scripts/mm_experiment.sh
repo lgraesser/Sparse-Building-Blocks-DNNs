c4f444d5348be2e7bf233355ec8f74523fe2dadf
evcu <evcu@users.noreply.github.com>
2017-12-11 13:39:08 -0500

    mm experiment script is creating data now

--- code/scripts/mm_experiment.sh
+++ code/scripts/mm_experiment.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/env bash
 
 # This line to get to the code file, no matter where it is called from
+mkn_s32=(32,256,128 32,512,256 32,1024,512 32,2048,1024 32,4096,2048 32,8192,4096)
+# mkn_s32=(1024,256,128 1024,512,256 1024,1024,512 1024,2048,1024 1024,4096,2048 1024,8192,4096)
+
+sparsity=(0 0.5 0.8 0.9 0.95 0.99)
+
+
 cd $( dirname "${BASH_SOURCE[0]}" )/../
 make bin/mm
-scripts/generate_sparse_mat.py 2,10 0.9 > data/a.mat
-scripts/generate_sparse_mat.py 10,5 0.9 > data/b.mat
-bin/mm data/a.mat data/b.mat
+
+
+for i in "${mkn_s32[@]}";
+do
+  IFS=","
+  set -- $i
+  for f in "${sparsity[@]}";
+  do
+    echo m=$1 k=$2 n=$3 sparsity=$f
+    FILENAME=data/$1_$2_$f.mat
+    if [ ! -e $FILENAME ]
+    then
+      scripts/generate_sparse_mat.py $1,$2 $f > $FILENAME
+    fi
+    FILENAME=data/$2_$3_$f.mat
+    if [ ! -e $FILENAME ]
+    then
+      scripts/generate_sparse_mat.py $2,$3 $f > $FILENAME
+    fi
+  done
+done
+
+# bin/mm data/a.mat data/b.mat -t -a cusparse -r
+# bin/mm data/a.mat data/b.mat -t -a denseblas -r
+# bin/mm data/a.mat data/b.mat -t -a sparseimp -r
