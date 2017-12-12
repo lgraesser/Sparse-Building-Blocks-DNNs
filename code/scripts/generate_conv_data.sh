307fca95f1dc7849d9058776fd5c87b418b5cca2
lgraesser <lhgraesser@gmail.com>
2017-12-12 15:45:17 -0500

    Fixes

--- code/scripts/generate_conv_data.sh
+++ code/scripts/generate_conv_data.sh
@@ -6,79 +6,79 @@ then
   mkdir ../$DATA_FOLDER/
 fi
 
-mat=224
-sparsity=0
-echo "Creating $mat x $mat matrix"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/$mat.mat
-
-mat=512
-echo "Creating $mat x $mat matrix"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/$mat.mat
-
-mat=1024
-echo "Creating $mat x $mat matrix"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/$mat.mat
-
-mat=2048
-echo "Creating $mat x $mat matrix"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/$mat.mat
-
-kernel=3
-sparsity=0
-sp=0
-echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+# mat=224
+# sparsity=0
+# echo "Creating $mat x $mat matrix"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/$mat.mat
+#
+# mat=512
+# echo "Creating $mat x $mat matrix"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/$mat.mat
+#
+# mat=1024
+# echo "Creating $mat x $mat matrix"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/$mat.mat
+#
+# mat=2048
+# echo "Creating $mat x $mat matrix"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/$mat.mat
 
 kernel=3
-sparsity=0.5
-sp=05
-echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
-
-kernel=3
-sparsity=0.9
-sp=09
-echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
-
-kernel=3
-sparsity=0.95
-sp=095
-echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
-
-kernel=3
-sparsity=0.99
-sp=099
-echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
-
-kernel=7
 sparsity=0
 sp=0
 echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k${mat}_${sp}.mat
 
-kernel=7
-sparsity=0.5
-sp=05
-echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
-
-kernel=7
-sparsity=0.9
-sp=09
-echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
-
-kernel=7
-sparsity=0.95
-sp=095
-echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
-
-kernel=7
-sparsity=0.99
-sp=099
-echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
-./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+# kernel=3
+# sparsity=0.5
+# sp=05
+# echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+#
+# kernel=3
+# sparsity=0.9
+# sp=09
+# echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+#
+# kernel=3
+# sparsity=0.95
+# sp=095
+# echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+#
+# kernel=3
+# sparsity=0.99
+# sp=099
+# echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+#
+# kernel=7
+# sparsity=0
+# sp=0
+# echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+#
+# kernel=7
+# sparsity=0.5
+# sp=05
+# echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+#
+# kernel=7
+# sparsity=0.9
+# sp=09
+# echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+#
+# kernel=7
+# sparsity=0.95
+# sp=095
+# echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
+#
+# kernel=7
+# sparsity=0.99
+# sp=099
+# echo "Creating $kernel x $kernel kernel, $sparsity sparsity"
+# ./generate_sparse_mat.py $mat,$mat $sparsity > ../data/k$mat_$sp.mat
